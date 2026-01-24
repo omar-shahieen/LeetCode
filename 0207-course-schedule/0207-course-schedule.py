68164f492447ef463be3aafdd8e314715bf4490e
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-01-24 16:47:59 +0200

    Time: 3 ms (86.92%), Space: 21.2 MB (14.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,39 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # 0 -> not visited  , 1-> process  2-> visited
-        NOT_VISITED = 0 
-        VISITING = 1
+        
+        NOT_VISITED = 0
+        VISITING = 1 
         VISITED = 2
 
-        visited= [NOT_VISITED] *  numCourses
-        adj =  defaultdict(list)
 
-        for a,b in prerequisites:
+        visited = [NOT_VISITED] * numCourses
+        adj = defaultdict(list)
+
+
+        for a ,b in prerequisites:
             adj[a].append(b)
 
 
         def dfs(v):
-            if  visited[v] == VISITING : return False # back edges
-            if  visited[v] == VISITED : return True # back edge
-            visited[v] = VISITING 
+            if visited[v] == VISITING : return False
+            if visited[v] == VISITED : return True
+
+
+            visited[v] =  VISITING
+
             for E in adj[v]:
-                    if not dfs(E):
-                        return False
+                if not dfs(E):
+                    return False
+
             visited[v] = VISITED
+
             return True
-                
+
         for v in range(numCourses):
-            if visited[v] == NOT_VISITED  :
+            if visited[v] == NOT_VISITED:
                 if not dfs(v):
                     return False
-        return True 
+
+        return True
+
+
