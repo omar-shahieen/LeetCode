091eb757b2a572a708e3104670a59643e9258ac9
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-02-02 18:24:13 +0200

    Time: 10 ms (58.05%), Space: 19.7 MB (32.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,42 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
-        comb=[]
-        def backtrack(target : int , idx : int):
-            if target == 0:
-                ans.append(comb[:])
+        """
+        idx -> 0 to n-1
+        path = []
+        res = [ [] ]
+        pathSum : int
+
+        if pathSum == target 
+            add path to res 
+        
+        start from 0 to n :
+            choose the number 
+
+            rec(idx  , pathSum + nums[i])
+            rec(idx + 1 , pathSum + nums[i])
+            pop the number from the path
+            
+        """
+
+        path = []
+        res = [] 
+        n = len(candidates)
+        
+        def backtrack(idx , pathSum):
+            if pathSum == target :
+                res.append(path[:])
+                return 
+            if idx == n or  pathSum > target : 
                 return
 
-            if idx >= len(candidates) or target < 0 : 
-                return
-            comb.append(candidates[idx])
-            backtrack(target - candidates[idx] , idx)
-            comb.pop()
-            backtrack(target  , idx + 1)
-        backtrack(target , 0 )
-        return ans
-            
+            # take the number with reuse
+            path.append(candidates[idx])
+            backtrack(idx , pathSum + candidates[idx])
+            path.pop()
+
+            # skip the number
+            backtrack(idx + 1, pathSum )
+
+
+        backtrack(0 , 0 )
+        return res
