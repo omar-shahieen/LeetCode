45615f83b14f34098814dac1783478da01994630
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-02-02 18:25:16 +0200

    Time: 12 ms (35.19%), Space: 19.7 MB (32.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        """
-        idx -> 0 to n-1
-        path = []
-        res = [ [] ]
-        pathSum : int
 
-        if pathSum == target 
-            add path to res 
-        
-        start from 0 to n :
-            choose the number 
-
-            rec(idx  , pathSum + nums[i])
-            rec(idx + 1 , pathSum + nums[i])
-            pop the number from the path
-            
-        """
 
         path = []
         res = [] 
