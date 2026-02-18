280e2195e8caac1188dd620c424d38c917405f3f
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-02-18 17:31:35 +0200

    Time: 2 ms (4.55%), Space: 19.5 MB (55.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,37 +1,29 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        n = len(matrix)  # rows
-        m = len(matrix[0]) # columns
-        col = -1 
-        top ,bottom = 0 , n - 1 
-        # seach for the row 
-        while top <= bottom :
-            mid = top + (bottom - top )//2      
-            if matrix[mid][0] == target:
-                return True
-            elif matrix[mid][0] >  target:
-                bottom = mid-1
-            else :
-                if mid == n-1 or (mid+1 < n and matrix[mid+1][0] >  target):
-                    col = mid
-                    break
-                else :
-                    top = mid + 1
-        # search for the column 
-        low , high = 0,m-1
-        while high >= low : 
-            mid = low + (high-low)//2
-            if matrix[col][mid] == target :
-                return True
-            elif matrix[col][mid] > target :
-                high = mid - 1
-            else:
-                low = mid+ 1   
-        return  False 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
+        top , bottom = 0 , len(matrix) - 1
+
+        row = -1 
+        while top <= bottom :
+            mid = top + (bottom - top) // 2
+
+            if matrix[mid][0] == target :
+                return True
+            elif matrix[mid][0] > target:
+                bottom = mid - 1
+            else :
+                top =  mid + 1  
+                row = mid       
+
+        l , r = 0 , len(matrix[0]) - 1
+
+        while l <= r :
+            m = l + (r-l)//2
+            if matrix[row][m] == target :
+                return True
+            elif matrix[row][m] > target:
+                r = m - 1
+            else :
+                l = m + 1
         
+        return False
