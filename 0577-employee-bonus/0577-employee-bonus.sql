281d8ec44d2da9b1039f5c1fdf156c8f3361bb7d
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-04-27 19:34:39 +0300

    Time: 1836 ms (5%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-SELECT e.name , b.bonus
-FROM Employee e
-LEFT JOIN Bonus b
-    on e.empId = b.empId
-WHERE b.bonus <1000 OR b.bonus is NULL
+-- name and bonus amount of each employee
+-- The employee has a bonus less than 1000.
+-- The employee did not get any bonus.
+select e.name , b.bonus  
+from employee e
+left join bonus b
+on e.empId = b.empId 
+where b.bonus < 1000 or b.bonus is NULL
