b69e9a40e6f46aecd4666aa8051ff7d5ed188bbb
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-04-27 18:14:17 +0300

    Time: 310 ms (25.48%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-SELECT name ,area,population
-FROM World
-WHERE area >= 3000000 OR population >= 25000000;
+-- name, population, and area
+-- big countries.
+-- it has an area of at least three million (i.e., 3000000 km2), or
+-- it has a population of at least twenty-five million (i.e., 25000000).
+
+select name , population , area
+from world 
+where area >= 3000000 or population >=25000000
