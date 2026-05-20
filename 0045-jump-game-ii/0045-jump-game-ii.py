d0bda1851d4d381b8b109134b4210e1f3a6b5389
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-05-20 16:40:43 +0300

    Time: 3 ms (89.45%), Space: 20.1 MB (34.72%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+        take a jump 
+        search for the best jump in the range 
+        take it
+        """
+        jumps = 0
+        curr_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest , i + nums[i])
+
+            if i== curr_end:
+                jumps +=1
+                curr_end = farthest
+
+        return jumps
+
+        
