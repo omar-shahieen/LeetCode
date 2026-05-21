f849caf5df2ba81afc537f47ef2f532b73425f18
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2026-05-21 21:14:09 +0300

    Time: 0 ms (100%), Space: 19.4 MB (17.11%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,5 +1,22 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
 
-        return format(int(a,2) + int(b,2), 'b')
+        res = []
+        i ,j = len(a)-1 , len(b)-1 
+        carry = 0 
+        while i >=0 or j >= 0 or  carry:
+            sum = carry 
+
+            if i >= 0 :
+                sum += int(a[i])
+                i-=1
+            if j >= 0 :
+                sum += int(b[j])
+                j-=1
+
+            res.append(str(sum%2))
+
+            carry = sum//2
+
+        return "".join(res[::-1])
         
